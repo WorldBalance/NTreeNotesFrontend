a81d3f0c61c1a0182a58ae57ebbc721afb5adf90
Vasilisa <40633911+VasilisaGoloborodko@users.noreply.github.com>
2020-11-02 18:03:29 +0200

    Fix: notes loading (#9)
    * Fix: lazy loading, note deletion

--- src/app/modules/general/notes/notes.component.ts
+++ src/app/modules/general/notes/notes.component.ts
@@ -11,7 +11,7 @@ import {NzMessageService} from 'ng-zorro-antd';
 import {TagModel} from '../../../models/tag.model';
 import {queryParamsPack, queryParamsUnpack} from 'src/utils/params'
 import {NoteModel} from '../../../models/note.model';
-import { toArray, truncateForHtml } from '../../../../utils/utils1';
+import {toArray, truncateForHtml} from '../../../../utils/utils1';
 
 @Component({
   selector: 'app-notes',
@@ -24,7 +24,7 @@ import { toArray, truncateForHtml } from '../../../../utils/utils1';
         query('div', style({transform: 'translatex(-100%)'})),
         query('div',
           stagger('15ms', [
-            animate('220ms', style ({transform: 'translateX(0)'}))
+            animate('220ms', style({transform: 'translateX(0)'}))
           ])
         )
       ])
@@ -34,7 +34,7 @@ import { toArray, truncateForHtml } from '../../../../utils/utils1';
         query('div', style({transform: 'translatex(-100%)'})),
         query('div',
           stagger('10ms', [
-            animate('250ms ease-in', style ({transform: 'translateX(0)'}))
+            animate('250ms ease-in', style({transform: 'translateX(0)'}))
           ])
         )
       ])
@@ -47,7 +47,7 @@ export class NotesComponent implements OnInit, OnDestroy {
   public tags$: Observable<object[]>;
   public searchTags: string[] = [];
   public notesSearchString: string;
-  public notes$: Observable<NoteModel[]>;
+  public notes: NoteModel[];
 
   private unsubscribe$ = new Subject<void>();
   private searchNoteDecouncer$: Subject<string> = new Subject();
@@ -64,15 +64,18 @@ export class NotesComponent implements OnInit, OnDestroy {
 
   public GetMoreNotesData() {
     if (this.store.data.notes.downloadMore) {
-      this.action.GetNotes(this.searchTags, this.notesSearchString);
+      this.action.GetNotes(this.searchTags, this.notesSearchString)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe((notes: NoteModel[]) => this.notes = this.notes.concat(notes));
     }
   }
 
   public ngOnInit(): void {
     this.getTags();
-    this.route.queryParams.pipe(takeUntil(this.unsubscribe$)).subscribe((params: Params) => {
-      this.notes$ = this.getNotes(params);
-    });
+    this.route.queryParams.pipe(
+      switchMap((params: Params) => this.getNotes(params)),
+      takeUntil(this.unsubscribe$)
+    ).subscribe((notes: NoteModel[]) => this.notes = notes);
     this.setupSearchNotesDebouncer();
   }
 
@@ -121,8 +124,8 @@ export class NotesComponent implements OnInit, OnDestroy {
   }
 
   public addNote(): void {
-    const queryParams = queryParamsPack( { tags: this.searchTags, search: this.notesSearchString } );
-    this.router.navigate(['/note'], { queryParams });
+    const queryParams = queryParamsPack({tags: this.searchTags, search: this.notesSearchString});
+    this.router.navigate(['/note'], {queryParams});
   }
 
   async FilterNotesTag(tagId) {
@@ -150,16 +153,17 @@ export class NotesComponent implements OnInit, OnDestroy {
       this.crudService.deleteNote(id).pipe(
         switchMap(() => this.route.queryParams),
         takeUntil(this.unsubscribe$)
-      ).subscribe((params: Params) => {
-        this.notes$ = this.getNotes(params)
+      ).subscribe(() => {
+        const deletedNote = this.notes.findIndex((note: NoteModel) => note.id === id);
+        this.notes.splice(deletedNote, 1);
       });
     }
   }
 
   // Обновить роут при фильтрации и запросах
   async refresh_url_search() {
-    const queryParams = queryParamsPack( { tags: this.searchTags, search: this.notesSearchString } );
-    return this.router.navigate(['/notes'], { queryParams });
+    const queryParams = queryParamsPack({tags: this.searchTags, search: this.notesSearchString});
+    return this.router.navigate(['/notes'], {queryParams});
   }
 
   private getTags(): void {
@@ -175,14 +179,14 @@ export class NotesComponent implements OnInit, OnDestroy {
     const params1 = queryParamsUnpack(params);
     this.notesSearchString = params1.search || '';
     this.searchTags = params1.tags || [];
-    return this.action.GetNotes(params1.tags, params1.search, { refresh: true })
+    return this.action.GetNotes(params1.tags, params1.search, {refresh: true})
       .pipe(
         map((notes: NoteModel[]) => {
           return notes.map((note: NoteModel) => {
-            let urlHtml = "";
+            let urlHtml = '';
             if (note.url) {
               const s1 = toArray(note.url).map((url1) => `<a href=${url1}>${truncateForHtml(url1, 50)}</a>`).join(', ');
-              urlHtml = "  (" + s1 + ")";
+              urlHtml = '  (' + s1 + ')';
             }
             return {...note, urlHtml};
           })
