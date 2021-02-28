7d818877a333c71231da31abc3852c914edd6282
Yuriy Litvin <J_DSoft@ukr.net>
2021-03-01 01:29:31 +0200

    Created domLoadUrls, updated old libraries

--- src/app/modules/general/item-view/item-view.component.ts
+++ src/app/modules/general/item-view/item-view.component.ts
@@ -1,3 +1,5 @@
+/// <reference types ="jquery"/>
+
 import {Component, OnInit} from '@angular/core';
 import {Note} from '../../../services/Store/NotesData.service';
 import {iif, Subject} from 'rxjs';
@@ -9,9 +11,9 @@ import {pluck, switchMap, takeUntil, tap} from 'rxjs/operators';
 import {cloneDeep} from 'lodash';
 import {TagsService} from '../../../services/tags.service';
 import {toArray, plainTextToHtmlWithBr} from '../../../../utils/utils1';
-import {NoteModel} from '../../../models/note.model';
+import {NoteModel, NoteWithTags} from '../../../models/note.model';
 import {Autolinker} from 'autolinker';
-declare const $: any;
+import {domLoadUrls} from '../../../../utils/utils1';
 
 
 @Component({
@@ -25,9 +27,6 @@ export class ItemViewComponent implements OnInit {
 
   public noteId: string;
   public note: Note;
-  public importUrls: Array<string> = ['https://ntree.online/s/libs/jquery/3.5.1/min.js',
-    'https://ntree.online/s/libs/magnific-popup/1.1.0/jquery.magnific-popup.min.js'
-  ];
 
   private unsubscribe$ = new Subject<void>();
 
@@ -55,10 +54,10 @@ export class ItemViewComponent implements OnInit {
         }
       ),
       takeUntil(this.unsubscribe$)
-    ).subscribe((note: NoteModel) => {
+    ).subscribe(async (note: NoteModel) => {
       if(this.noteId){
         this.tagsService.mapTagsToNote(note).subscribe(
-          (noteWithTags: NoteModel) => {
+          (noteWithTags: NoteWithTags) => {
             const initialNote: Note = cloneDeep({...noteWithTags, tags: noteWithTags.tags || []});
             if(initialNote.url){
               initialNote.url = toArray(initialNote.url);
@@ -75,50 +74,12 @@ export class ItemViewComponent implements OnInit {
           }
         );
       }
-    });
-    // external import
-      this.importUrls.forEach(url => {
-        const node = document.createElement('script');
-        node.src = url;
-        node.type = 'text/javascript';
-        node.async = false;
-        document.getElementsByTagName('head')[0].appendChild(node);
-      });
 
-      this.createGallery()
-  }
-
-  private createGallery(){
-    // checking import JQuery and Magnific-Popup libs
-    // if everything ok -> use Magnific for creating Gallery
-    let countTrying = 0;
-
-    const timer = setInterval(async () => {
-      countTrying++;
-      if(typeof $ === 'function') {
-        if (typeof $.magnificPopup !== 'undefined') {
-            await $('.popup-gallery').magnificPopup({
-            delegate: 'a',
-            type: 'image',
-            tLoading: 'Loading image #%curr%...',
-            mainClass: 'mfp-img-mobile',
-            gallery: {
-              enabled: true,
-              navigateByImgClick: true,
-              preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-            },
-            image: {
-              tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            }
-          });
-          clearInterval(timer);
-        }
+      // dynamic loading libraries if we need it
+      if (note.files && note.files.length > 0) {
+        await domLoadUrls(importUrls, { parentId: "bodyBegin" }).then(() => createGallery());
       }
-      if(countTrying > 20){
-        clearInterval(timer);
-        alert('You have trouble with Gallery or uploading data from server, restart page, please.')
-      };
-    }, 200);
+    });
   }
 
   public editNote(id: string): void{
@@ -128,4 +89,40 @@ export class ItemViewComponent implements OnInit {
   public displayText(text: string){
     return Autolinker.link(plainTextToHtmlWithBr(text));
   }
+}
+
+
+const importUrls: Array<string> = [
+  'https://ntree.online/s/libs/jquery/3.5.1/min.js',
+  'https://ntree.online/s/libs/magnific-popup/1.1.0/magnific-popup.css',
+  'https://ntree.online/s/libs/magnific-popup/1.1.0/jquery.magnific-popup.min.js',
+];
+
+function createGallery(){
+  // checking import JQuery and Magnific-Popup libs
+  // if everything ok -> use Magnific for creating Gallery
+  let countTrying = 0;
+
+  const timer = setInterval(async () => {
+    if (jQuery && jQuery('.popup-gallery').length && (jQuery as any).magnificPopup) {
+      clearInterval(timer);
+      (jQuery as any)('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+          enabled: true,
+          navigateByImgClick: true,
+          preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+          tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        }
+      });
+    } else if (++countTrying > 100) {
+      clearInterval(timer);
+      alert('You have trouble with downloading libraries from server, restart page, please.')
+    };
+  }, 50);
 }
--- src/utils/utils1.ts
+++ src/utils/utils1.ts
@@ -95,3 +95,65 @@ export function decodeHtml(str: HtmlEncoded) : Html {
 export function plainTextToHtmlWithBr(str: PlainText) : Html {
     return str.split("\n").map(encodeHtml).join("<br>");
 }
+
+
+// sleep, wait, delay
+export function sleep(ms: number): Promise<void> {
+    return new Promise(resolve => void setTimeout(resolve, ms));
+}
+
+
+export interface domLoadUrlOpt {
+    parentId?: string; // default: <head>
+    removeChildren?: boolean; // default: false, only if <parentId> is valid
+    sleepMs?: number; // default: 10
+}
+
+export function domLoadUrls(urls: string[], opt?: domLoadUrlOpt): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+        const parentId = opt && opt.parentId;
+        const parentNodeUser = parentId && document.getElementById(parentId);
+        if (parentId && !parentNodeUser) {
+            throw new Error(`domLoadUrls, not found node with id: '${parentId}'`);
+        }
+        if (opt && opt.removeChildren) {
+            removeChildren(parentNodeUser);
+        }
+        const sleepMs = isDefined(opt && opt.sleepMs) && opt.sleepMs || 10;
+
+        const parentNode = parentNodeUser || document.getElementsByTagName('head')[0];
+        for (const url of urls) {
+            let node;
+            if (url.endsWith(".js")) {
+                const node1 = document.createElement('script');
+                node1.src = url;
+                node1.type = 'text/javascript';
+                node1.async = false;
+                node = node1;
+            } else if (url.endsWith(".css")) {
+                const node1 = document.createElement('link');
+                node1.rel = 'stylesheet';
+                node1.href = url;
+                node = node1;
+            } else {
+                throw new Error(`domLoadUrls, unsupported extension of loaded file, expected: {OneOf: .js, .css}, actual: '${url}'`);
+            }
+            parentNode.appendChild(node);
+
+            sleepMs && await sleep(sleepMs);
+        }
+        resolve();
+    });
+}
+
+
+export function removeChildren(parent: HTMLElement) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+export function isDefined(value: any): boolean {
+    return (typeof(value) !== 'undefined');
+}
